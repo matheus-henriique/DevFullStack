add63c0f0f7c9d508fbb026cd9c64ec8039ed5ad
EA - Matheus Henrique Lopes Rodrigues <matheus_lopes01@educadventista.org>
2023-10-10 21:44:05 -0300

    repetição no handlebars

---
+++ 3_HANDLEBASS/5_condicionais/index.js
@@ -0,0 +1,32 @@
+const PORT = 3000;
+
+const express = require('express');
+const exphbs = require('express-handlebars');
+const app = express();
+
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+
+app.get('/', (req, res) => {
+    
+    const usuario = {
+        nome: "Matheus",
+        profissao: "Programador",
+        idade: 19
+    }
+
+    const auth = true;
+    
+    res.render('home', {usuario, auth});
+});
+
+app.get('/dashboard', (req, res) => {
+    const items = [`item a`, `item b`, `item c`]
+    res.render('dashboard', {items})
+});
+
+app.listen(PORT, ()=>{
+    console.log("Rodando");
+}); 
